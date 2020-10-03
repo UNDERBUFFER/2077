1037d8bccb7e6daebfa402111114c11e6d2f9bf9
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-10-03 20:09:51 +0300

    working fuctional

--- conf.js
+++ conf.js
@@ -5,3 +5,14 @@ const MAP_WIDTH = 320
 const MAP_HEIGHT = 320
 const BLOCK_WIDTH = 80
 const BLOCK_HEIGHT = 80
+
+Array.prototype.remove = function() {
+    let what, a = arguments, L = a.length, ax
+    while (L && this.length) {
+        what = a[--L]
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1)
+        }
+    }
+    return this
+}
--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 'use strict';
 
+
 const BLOCKS = []
+
 
 function newBlock() {
     const innerDiv = document.createElement('div')
     innerDiv.className = 'block'
-    innerDiv.innerHTML += '<center><h1>2</h1></center>';
+    innerDiv.innerHTML += '<center><h1>2</h1></center>'
 
     for (let i = 0; i < 100; i++) {
         innerDiv.style.marginLeft = `${getRandomInteger(0, ( MAP_WIDTH / BLOCK_WIDTH ) - 1) * BLOCK_WIDTH}px`
@@ -23,12 +25,13 @@ function newBlock() {
 
 }
 
+
 function checkList(innerDiv, marginLeft=null, marginTop=null) {
+    const passedBlock = {
+        marginLeft: marginLeft === null || marginTop === null ? innerDiv.style.marginLeft : marginLeft,
+        marginTop: marginLeft === null || marginTop === null ? innerDiv.style.marginTop: marginTop
+    }
     for (let block of BLOCKS) {
-        const passedBlock = {
-            marginLeft: marginLeft === null || marginTop === null ? innerDiv.style.marginLeft : marginLeft,
-            marginTop: marginLeft === null || marginTop === null ? innerDiv.style.marginTop: marginTop
-        }
         if ( passedBlock.marginLeft == block.style.marginLeft &&
             passedBlock.marginTop == block.style.marginTop ) {
             return false
@@ -36,6 +39,39 @@ function checkList(innerDiv, marginLeft=null, marginTop=null) {
     }
     return true
 }
+
+
+function findBlockByCoordinates(innerDiv, marginLeft=null, marginTop=null) {
+    const passedBlock = {
+        marginLeft: marginLeft === null || marginTop === null ? innerDiv.style.marginLeft : marginLeft,
+        marginTop: marginLeft === null || marginTop === null ? innerDiv.style.marginTop: marginTop
+    }
+    for (let block of BLOCKS) {
+        if ( passedBlock.marginLeft == block.style.marginLeft &&
+            passedBlock.marginTop == block.style.marginTop ) {
+            return block
+        }
+    }
+    return false
+}
+
+
+function insertHTML(block, text) {
+    block.innerHTML = `<center><h1>${text}</h1></center>`
+}
+
+
+function clearBlockHistory(block) {
+    BLOCKS.remove(block)
+    document.getElementById('map').removeChild( block )
+}
+
+
+function connectBlocks(someBlock, mainBlock) {
+    clearBlockHistory( someBlock )
+    insertHTML( mainBlock, Number(mainBlock.innerText) * 2 )
+}
+
 
 function moveBlocks(keyName) {
     const sortedBlocks = [...BLOCKS].sort( sortingFunctions[keyName] )
@@ -45,36 +81,53 @@ function moveBlocks(keyName) {
             case 'ArrowUp':
                 while ( style.marginTop != '0px' ) {
                     let newValue = `${parseCSSValue(style.marginTop) - BLOCK_HEIGHT}px`
-                    if ( !checkList( null, style.marginLeft, newValue ) ) break
-                    style.marginTop = newValue
+                    let notEmptyBlock = false
+                    if ( notEmptyBlock = findBlockByCoordinates( null, style.marginLeft, newValue ) ) {
+                        if (notEmptyBlock.innerText == block.innerText) connectBlocks( block, notEmptyBlock )
+                        else break
+                    }
+                    else style.marginTop = newValue
                 }
                 break
             case 'ArrowDown':
                 while ( style.marginTop != `${MAP_HEIGHT - BLOCK_HEIGHT}px` ) {
                     let newValue = `${parseCSSValue(style.marginTop) + BLOCK_HEIGHT}px`
-                    if ( !checkList( null, style.marginLeft, newValue ) ) break
-                    style.marginTop = newValue
+                    let notEmptyBlock = false
+                    if ( notEmptyBlock = findBlockByCoordinates( null, style.marginLeft, newValue ) ) {
+                        if (notEmptyBlock.innerText == block.innerText) connectBlocks( block, notEmptyBlock )
+                        else break
+                    }
+                    else style.marginTop = newValue
                 }
                 break
             case 'ArrowLeft':
                 while ( style.marginLeft != '0px' ) {
                     let newValue = `${parseCSSValue(style.marginLeft) - BLOCK_WIDTH}px`
-                    if ( !checkList( null, newValue, style.marginTop ) ) break
-                    style.marginLeft = newValue
+                    let notEmptyBlock = false
+                    if ( notEmptyBlock = findBlockByCoordinates( null, newValue, style.marginTop ) ) {
+                        if (notEmptyBlock.innerText == block.innerText) connectBlocks( block, notEmptyBlock )
+                        else break
+                    }
+                    else style.marginLeft = newValue
                 }
                 break
             case 'ArrowRight':
                 while ( style.marginLeft != `${MAP_WIDTH - BLOCK_WIDTH}px` ) {
                     let newValue = `${parseCSSValue(style.marginLeft) + BLOCK_WIDTH}px`
-                    if ( !checkList( null, newValue, style.marginTop ) ) break
-                    style.marginLeft = newValue
+                    let notEmptyBlock = false
+                    if ( notEmptyBlock = findBlockByCoordinates( null, newValue, style.marginTop ) ) {
+                        if (notEmptyBlock.innerText == block.innerText) connectBlocks( block, notEmptyBlock )
+                        else break
+                    }
+                    else style.marginLeft = newValue
                 }
                 break
         }        
     }
 }
 
+
 document.addEventListener('keydown', (event) => {
     moveBlocks(event.key)
+    newBlock()
 })
-
